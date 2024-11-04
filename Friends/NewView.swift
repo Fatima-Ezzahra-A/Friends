0bcec91735966df7dd60088f9e9a0c3ef285490f
aazzouz.fatima.ezzahra <aazzouz.fatima.ezzahra@gmail.com>
2024-11-04 16:23:34 +0100

    ....

---
+++ Friends/NewView.swift
@@ -0,0 +1,19 @@
+//
+//  NewView.swift
+//  Friends
+//
+//  Created by aazzouz fatima ezzahra on 04/11/24.
+//
+
+import SwiftUI
+
+struct NewView: View {
+    var body: some View {
+        Text("Hello, Friends!")
+        
+    }
+}
+
+#Preview {
+    NewView()
+}
